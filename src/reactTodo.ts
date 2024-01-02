cadad3497d73cb8b9062224d5bc934dbfe7582f1
arthurfaraujo <arthurfelipe5567@gmail.com>
2024-01-02 12:43:18 -0300

    feat: move todo routing logic to its own file
    also add error handling to server

--- src/reactTodo.ts
+++ src/reactTodo.ts
@@ -1,8 +1,12 @@
+
+import 'express-async-errors'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import morgan from 'morgan'
 import userRouter from './routes/user'
+import todoRouter from './routes/todo'
+import { notFound, errorHandler } from './middlewares/error'
 
 const PORT = process.env.PORT || 3000
 
@@ -13,19 +17,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/todos', (req, res) => {
-  res.json({
-    todos: [
-      {
-        index: new Date().getTime(),
-        content: 'Buy milk',
-        done: false
-      }
-    ]
-  })
-})
+app.use('/api/todo', todoRouter)
 
 app.use('/api/user', userRouter)
+
+app.use(notFound, errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
