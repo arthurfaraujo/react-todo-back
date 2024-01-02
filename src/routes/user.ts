c48d296ff174d5f48cc0ea61891134dd5ad1a3db
arthurfaraujo <arthurfelipe5567@gmail.com>
2024-01-02 12:42:40 -0300

    feat: add jwt

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,17 +1,46 @@
 import { Router } from 'express'
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
 const router = Router()
+const JWT_SECRET = process.env.JWT_SECRET || 'secret'
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const newUser = req.body
 
-  const user = User.create(newUser)
+  const user = await User.create(newUser)
 
-  res.json({
-    created: true,
-    user
+  res.status(201).json({
+    created: user ? true : false,
+    user: newUser
   })
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findUnique(email)
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ authenticated: false, message: 'User not found' })
+  }
+
+  if (user.password !== password) {
+    return res
+      .status(400)
+      .json({ authenticated: false, message: 'Invalid password' })
+  }
+
+  res
+    .status(200)
+    .json({
+      authenticated: true,
+      message: 'User authenticated',
+      token: jwt.sign({ id: user.id, nickname: user.nickname }, JWT_SECRET)
+    })
 })
 
 export default router
