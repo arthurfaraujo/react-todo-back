7fda323568732d2d76c09d21725c0b1c7a870acf
arthur <arthurfelipe5567@gmail.com>
2024-01-04 19:09:10 -0300

    feat: add interface to auth middleware

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,16 +1,22 @@
+import { Request } from 'express'
+
 export interface TodoItf {
-  id?: string;
-  title: string;
-  completed: boolean;
-  userId: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+  id?: string
+  title: string
+  completed: boolean
+  userId: string
+  createdAt?: Date
+  updatedAt?: Date
 }
 
 export interface UserItf {
-  id?: string;
-  nickname: string;
-  email: string;
-  password: string;
-  createdAt?: Date;
+  id?: string
+  nickname: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export interface AuthenticatedRequest extends Request {
+  userId?: string
 }
