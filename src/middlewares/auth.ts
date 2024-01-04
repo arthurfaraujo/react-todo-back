79165fd37968f128185ff65b140b9e0b43759e11
arthur <arthurfelipe5567@gmail.com>
2024-01-04 19:09:56 -0300

    feat: add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import { Response, NextFunction } from 'express'
+import { AuthenticatedRequest } from '../interfaces/interfaces'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+export function auth(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): void | Response {
+  try {
+    const token = req.headers.authorization?.split(' ')[1]
+
+    if (!token) return res.status(401).json({ error: 'No token' })
+
+    const auth = jwt.verify(token, process.env.JWT_SECRET as string)
+
+    if (auth) {
+      req.userId = (auth as JwtPayload).id
+      return next()
+    }
+  } catch (e) {
+    res.status(401).json({ error: 'Unauthenticated' })
+  }
+}
